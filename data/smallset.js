68322377de338a9bf40e7bd80e3adb0d92c9c257
Ben Rudolph <benrudolph@rescomp-12-241233.stanford.edu>
2012-09-29 20:00:58 -0700

    First commit

---
+++ data/smallset.js
@@ -0,0 +1,101 @@
+var refugees = [
+        {
+            Asylum: "Afghanistan",
+            Origin: "Eritrea",
+            Year: 2010,
+            Refugees: 0,
+            RefugeesAssisted: 0,
+            RefugeeLikeSituation: 0,
+            RefugeeLikeSituationAssisted: 0
+        },
+        {
+            Asylum: "Afghanistan",
+            Origin: "Iraq",
+            Year: 2010,
+            Refugees: 6,
+            RefugeesAssisted: 6,
+            RefugeeLikeSituation: 6,
+            RefugeeLikeSituationAssisted: 6
+        },
+        {
+            Asylum: "Afghanistan",
+            Origin: "Iran",
+            Year: 2010,
+            Refugees: 30,
+            RefugeesAssisted: 30,
+            RefugeeLikeSituation: 30,
+            RefugeeLikeSituationAssisted: 0
+        },
+        {
+            Asylum: "Afghanistan",
+            Origin: "Pakistan",
+            Year: 2010,
+            Refugees: 7,
+            RefugeesAssisted: 7,
+            RefugeeLikeSituation: 6398,
+            RefugeeLikeSituationAssisted: 8
+        },
+        {
+            Asylum: "Albania",
+            Origin: "China",
+            Year: 2010,
+            Refugees: 6,
+            RefugeesAssisted: 6,
+            RefugeeLikeSituation: 6,
+            RefugeeLikeSituationAssisted: 6
+        },
+        {
+            Asylum: "Albania",
+            Origin: "Czech Republic",
+            Year: 2010,
+            Refugees: 2,
+            RefugeesAssisted: 2,
+            RefugeeLikeSituation: 2,
+            RefugeeLikeSituationAssisted: 2
+        },
+        {
+            Asylum: "Albania",
+            Origin: "Egypt",
+            Year: 2010,
+            Refugees: 3,
+            RefugeesAssisted: 3,
+            RefugeeLikeSituation: 3,
+            RefugeeLikeSituationAssisted: 3
+        },
+        {
+            Asylum: "Albania",
+            Origin: "Iraq",
+            Year: 2010,
+            Refugees: 5,
+            RefugeesAssisted: 5,
+            RefugeeLikeSituation: 5,
+            RefugeeLikeSituationAssisted: 5
+        },
+        {
+            Asylum: "Albania",
+            Origin: "Iran",
+            Year: 2010,
+            Refugees: 1,
+            RefugeesAssisted: 1,
+            RefugeeLikeSituation: 1,
+            RefugeeLikeSituationAssisted: 1
+        },
+        {
+            Asylum: "Albania",
+            Origin: "Libyan Arab Jamahiriya",
+            Year: 2010,
+            Refugees: 1,
+            RefugeesAssisted: 1,
+            RefugeeLikeSituation: 1,
+            RefugeeLikeSituationAssisted: 1
+        },
+        {
+            Asylum: "Albania",
+            Origin: "Montenegro",
+            Year: 2010,
+            Refugees: 1,
+            RefugeesAssisted: 1,
+            RefugeeLikeSituation: 1,
+            RefugeeLikeSituationAssisted: 1
+        },
+]
---
+++ vis/refugee.js
@@ -0,0 +1,172 @@
+(function(window) {
+  var data,
+      xy = d3
+            .geo
+            .equirectangular()
+            .scale($('#map_container').width())
+            .translate([$('#map_container').width() / 2, $('#map_container').height() / 2]),
+      path = d3
+              .geo
+              .path()
+              .projection(xy),
+      svg = d3
+              .select('#map_container')
+              .append('svg:svg')
+              .attr("pointer-events", "all")
+              .append("svg:g")
+              .call(d3.behavior.zoom().on("zoom", redraw))
+              .append("svg:g"),
+      countries = svg
+                    .append('svg:g')
+                    .attr('id', 'countries'),
+      nodes = svg
+                    .append('svg:g')
+                    .attr('id', 'circles'),
+      maxWidth = 30,
+      minWidth = 1
+
+
+
+  /* World Map */
+
+  var area = d3.svg.area()
+    .x0(function(d) { return d.x0 })
+    .x1(function(d) { return d.x1 })
+    .y0(function(d) { return d.y0 })
+    .y1(function(d) { return d.y1 })
+
+  var helper = {
+    getCenterPoint: function(element) {
+      var bbox = element.getBBox()
+      return {
+        x0: bbox.x + (bbox.width / 2),
+        x1: bbox.x + (bbox.width / 2),
+        y0: bbox.y + (bbox.height / 2),
+        y1: bbox.y + (bbox.height / 2)
+      }
+    }
+  }
+
+  refugees = refugees.filter(function(d) { return d.Year == 2010 })
+
+  var containers = nodes.selectAll('.node')
+    .data(refugees)
+    .enter()
+    .append("g")
+    .attr("class", "node")
+
+  var totalRefugees = {}
+  var maxRefugees = 0
+
+  for (var index = 0; index < refugees.length; index++) {
+    var datum = refugees[index]
+    var key = datum.Asylum.toUpperCase()
+    if (totalRefugees[key]) {
+      totalRefugees[key] += datum.RefugeeLikeSituation
+    } else {
+      totalRefugees[key] = datum.RefugeeLikeSituation
+    }
+  }
+
+  for (var key in totalRefugees) {
+    if (totalRefugees.hasOwnProperty(key) && totalRefugees[key] > maxRefugees)
+      maxRefugees = totalRefugees[key]
+  }
+
+  countries.selectAll('path')
+    .data(window.countries_data.features) // get the data here: https://gist.github.com/2969317
+    .enter()
+    .append('svg:path')
+    .attr('d', path)
+   .attr('fill', function(d) {
+      if (!totalRefugees[d.properties.name.toUpperCase()])
+        return "white"
+      var lightness = totalRefugees[d.properties.name.toUpperCase()] / maxRefugees
+
+      return d3.hsl(0, .6, 1 - (lightness))
+   })
+   .attr('stroke', 'rgba(0,0,0, 1)')
+   .attr('stroke-width', 1)
+   .attr("id", function(d) {
+     if (d.properties.name)
+       return d.properties.name.toUpperCase().toUpperCase().replace(/ /g, "_")
+   })
+   .on("mouseover", brighten(1))
+   .on("mouseout", brighten(0))
+
+  containers
+    .append('svg:path')
+    .attr('d', function(d) {
+      var asylum = d3.select('#' + d.Asylum.toUpperCase().replace(/ /g, "_"))
+      var origin = d3.select('#' + d.Origin.toUpperCase().replace(/ /g, "_"))
+      if (d.Asylum === "United States") {
+        console.log(d.Asylum)
+        console.log(d.Origin)
+      }
+      if (!asylum || !origin || !asylum[0] || !origin[0] || !asylum[0][0] || !origin[0][0])
+        return
+      var end = helper.getCenterPoint(asylum[0][0])
+      var start = helper.getCenterPoint(origin[0][0])
+      var width = ((d.RefugeeLikeSituation / totalRefugees[d.Asylum.toUpperCase()]) * maxWidth) + minWidth
+      var newStart = createTriangleFromLine(start, end, width)
+      return area([newStart, end])
+    })
+    .attr('class', function(d) { d.Asylum.toUpperCase() })
+    .attr("display", "none")
+    .attr("fill", "black")
+    .attr('stroke', 'rgba(0,0,0,0)')
+    .attr('stroke-width', 0)
+
+
+  function brighten(opacity) {
+    return function(g, i) {
+      $("#info").text(g.properties.name + ": " + totalRefugees[g.properties.name.toUpperCase()])
+      nodes.selectAll("g.node path")
+        .filter(function(d) {
+          return d.Asylum.toUpperCase() != g.properties.name.toUpperCase()
+        })
+      nodes.selectAll("g.node path")
+        .filter(function(d) {
+          return d.Asylum.toUpperCase() == g.properties.name.toUpperCase()
+        })
+        .transition()
+          .style("display", function(d) { if (opacity === 1) return "inline" })
+          .style("stroke-width", 1)
+    }
+  }
+
+  function createTriangleFromLine(start, end, base) {
+    var xLength = end.x0 - start.x0
+    var yLength = -(end.y0 - start.y0)
+    var theta = (Math.PI / 2) - Math.atan(yLength / xLength)
+    var hypotenuse = base / 2
+    var yBaseLength = hypotenuse * Math.sin(theta)
+    var xBaseLength = hypotenuse * Math.cos(theta)
+    return {
+      x0: start.x0 - xBaseLength,
+      x1: start.x1 + xBaseLength,
+      y0: start.y0 - yBaseLength,
+      y1: start.y1 + yBaseLength
+    }
+  }
+
+
+
+  function focus() {
+    return function(g, i) {
+      console.log("Focusuing")
+    }
+  }
+
+  function redraw() {
+    console.log("here", d3.event.translate, d3.event.scale);
+    svg.attr("transform",
+        "translate(" + d3.event.translate + ")"
+        + " scale(" + d3.event.scale + ")");
+    svg.selectAll("g.node path")
+      .attr("transform", "scale(1)")
+  }
+
+
+
+})(this);
